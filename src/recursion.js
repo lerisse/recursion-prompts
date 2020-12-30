f4f928f930d51bf01d60173cd64b0a15103a8158
Tristan Lerisse <tristanlerisse@gmail.com>
2020-12-30 13:33:32 -0500

    Exercise 7 Complete Starting to get difficult to understand

--- src/recursion.js
+++ src/recursion.js
@@ -95,7 +95,18 @@ var range = function(x, y) {
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
+
+//
 var exponent = function(base, exp) {
+  if (exp === 0) {
+    return 1;
+  } if (exp === 1) {
+    return base;
+    } if (exp > 0) {
+        return (base * exponent(base, exp-1));
+      } else {
+        return 1 / exponent(base, -exp);
+      }
 };
 
 // 8. Determine if a number is a power of two.
